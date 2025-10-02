21e44dc8a172cf4e467eda9c0772de2d72232be4
bhavitha7604 <bhavitha1976@gmail.com>
2025-10-02 14:43:36 +0530

    README.m

--- Week-01/README.m
+++ Week-01/README.m
@@ -1,19 +1,19 @@
-# Week 01 - Personal Portfolio Website
+ Week 01 - Personal Portfolio Website
 
-This is my *Week 01 task* for the Edulumos Internship.
+This is my Week 01 task for the Edulumos Internship.
 
-## 📌 Project Description
+ 📌 Project Description
 A simple personal portfolio website built with *HTML* and *CSS*.  
 The website includes:
 - 🧑 About Me section  
 - 💻 Projects section  
 - 📞 Contact details  
 
-## 🗂 Files
+ 🗂 Files
 - index.html → main webpage structure  
 - style.css → styling and layout  
 
-## 🚀 How to run
+ 🚀 How to run
 1. Download the project files (or clone the repository).  
 2. Open index.html in any web browser.  
 3. The portfolio website will be displayed.  
